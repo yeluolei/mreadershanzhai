46c7537e77915c120a359f6ae29cc460b7a9dde8
zxykobezxy <zxykobezxy@e3e24428-ab4a-6b2f-fdd4-c73a3daa3618>
2010-05-18 08:54:09 +0000

    the first version

---
+++ trunk/MReader/MReader/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MReader.Models;
+
+
+namespace MReader.Controllers
+{
+    [HandleError]
+    public class HomeController : Controller
+    {
+        BookRepository db = new BookRepository();
+        
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult list()
+        {
+            return View();
+        }
+
+
+        [Authorize]
+        public ActionResult BookView( int? page,int? ID)
+        {
+            Book book = db.GetBookbyID( ID ?? 1 );
+            int temppage = page ?? 1;
+            string URL = book.Content + temppage.ToString()+".png"; 
+           
+            BookPageFormModel bh = new BookPageFormModel(book,temppage,URL);
+            
+            return View(bh);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult BookView(BookPageFormModel bookModel)
+        { 
+           
+            return BookView(bookModel.pageIndex, Convert.ToInt32(Request.Form["ID"]));
+        }
+
+    }
+
+  
+}
---
+++ trunk/MReader/MReader/Views/Home/list.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage<dynamic>" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+	list
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h2>list</h2>
+    <ul id="acc1" class="ui-accordion-container">
+	<li>
+		<div class="ui-accordion-left"></div>
+		<a class="ui-accordion-link acc1">1a
+			<span class="ui-accordion-right"></span></a>
+		<div>
+ 
+			<ul class="ui-accordion-container" id="acc2">
+				<li>
+					<div class="ui-accordion-left"></div>
+					<a class="ui-accordion-link acc2">2a
+						<span class="ui-accordion-right"></span></a>
+					<div>
+						2a<br />
+						2a<br />
+ 
+						2a<br />
+					</div>
+				</li>
+				<li>
+					<div class="ui-accordion-left"></div>
+					<a class="ui-accordion-link acc2">2b
+						<span class="ui-accordion-right"></span></a>
+					<div>
+						2b<br />
+ 
+						2b<br />
+						2b<br />
+					</div>
+				</li>
+			</ul>
+		</div>
+	</li>
+	<li>
+ 
+		<div class="ui-accordion-left"></div>
+		<a class="ui-accordion-link acc1">1b
+			<span class="ui-accordion-right"></span></a>
+		<div>
+			1b<br />
+			1b<br />
+			1b<br />
+		</div>
+ 
+	</li>
+</ul>
+
+
+</asp:Content>
